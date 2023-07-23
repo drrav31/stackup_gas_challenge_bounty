22e0f1169e14eda118a3c283227f47187d6885c9
drrav31 <47712868+drrav31@users.noreply.github.com>
2023-07-23 08:36:49 +0530

    Update gasChallenge.sol
    Add comments

--- contracts/gasChallenge.sol
+++ contracts/gasChallenge.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.0;
 
 contract gasChallenge {
     //Implement Fixed-Size Array Technique Here
+    //Fix the array size to 10 as only 10 elements are present
     uint[10] numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
     //Function to check for sum of array
@@ -24,9 +25,15 @@ contract gasChallenge {
     //Implement Remaining Gas Optimization Techniques Here
     //Sum of elements in the numbers array should equal 0
     function optimizedFunction() public {
+        //Cache the array length
         uint arr_length = numbers.length;
+
         for (uint i = 0; i < arr_length; ) {
+            //set value of each array element to 0
             numbers[i] = 0;
+
+            //increment value of i in unchecked block to save gas.
+            //as value of i is bounded by the fixed-array length,it'll never overflow
             unchecked {
                 ++i;
             }
